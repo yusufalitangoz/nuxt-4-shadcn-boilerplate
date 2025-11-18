11b313373abb967e082d9fc2b31768905967f5e8
yali <tangozyusufali@gmail.com>
2025-11-18 12:11:08 +0300

    refactor(input): reorder props definition and remove tel formatting

--- app/components/ui/input/Input.vue
+++ app/components/ui/input/Input.vue
@@ -3,9 +3,9 @@ import type { HTMLAttributes } from "vue";
 import { useVModel } from "@vueuse/core";
 
 const props = defineProps<{
+  class?: HTMLAttributes["class"];
   defaultValue?: string | number;
   modelValue?: string | number;
-  class?: HTMLAttributes["class"];
   type?: string;
 }>();
 
@@ -14,28 +14,19 @@ const emits = defineEmits<{
 }>();
 
 const modelValue = useVModel(props, "modelValue", emits, {
-  passive: true,
   defaultValue: props.defaultValue,
+  passive: true,
 });
 const showPassword = ref(false);
-
-watchEffect(() => {
-  if (props.type === "tel" && modelValue.value) {
-    modelValue.value = modelValue.value
-      .toString()
-      .replaceAll(/\u00A0/g, "")
-      .replaceAll(" ", "");
-  }
-});
 </script>
 
 <template>
   <div class="relative">
     <input
       v-model="modelValue"
+      :type="showPassword ? 'text' : type"
       data-slot="input"
       v-bind="$attrs"
-      :type="showPassword ? 'text' : type"
       :class="
         cn(
           'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
