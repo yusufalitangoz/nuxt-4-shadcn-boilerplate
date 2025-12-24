ad61263f295dd058243bb148826bf860e963c0a3
yali <tangozyusufali@gmail.com>
2025-12-24 15:40:44 +0300

    style: add w-max to Logo link

--- app/components/layout/Logo.vue
+++ app/components/layout/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLinkLocale to="/" class="shrink-0">
+  <NuxtLinkLocale to="/" class="shrink-0 w-max">
     <NuxtImg src="/logo.png" width="40" height="40" alt="Logo" />
   </NuxtLinkLocale>
 </template>
