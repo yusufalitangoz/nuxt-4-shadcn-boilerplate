241fab85af39c6a213ab7143fabb4f52080f5b36
yali <tangozyusufali@gmail.com>
2025-12-25 18:58:51 +0300

    feat: add theme binding to sonner

--- app/components/ui/sonner/Sonner.vue
+++ app/components/ui/sonner/Sonner.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import type { Theme } from "vue-sonner/src/packages/types.js";
 import type { ToasterProps } from "vue-sonner";
 import { Toaster as Sonner } from "vue-sonner";
 
@@ -8,35 +9,32 @@ const props = withDefaults(defineProps<ToasterProps>(), {
 </script>
 
 <template>
-  <Sonner
-    :class="cn('toaster group', props.class)"
-    :style="{
-      '--normal-bg': 'var(--popover)',
-      '--normal-text': 'var(--popover-foreground)',
-      '--normal-border': 'var(--border)',
-      '--border-radius': 'var(--radius)',
-    }"
-    v-bind="props"
-  >
-    <template #success-icon>
-      <Icon name="lucide:circle-check" class="size-4" />
-    </template>
-    <template #info-icon>
-      <Icon name="lucide:info" class="size-4" />
-    </template>
-    <template #warning-icon>
-      <Icon name="lucide:triangle-alert" class="size-4" />
-    </template>
-    <template #error-icon>
-      <Icon name="lucide:octagon-x" class="size-4" />
-    </template>
-    <template #loading-icon>
-      <div>
-        <Icon name="lucide:loader" class="size-4 animate-spin" />
-      </div>
-    </template>
-    <template #close-icon>
-      <Icon name="lucide:x" class="size-4" />
-    </template>
-  </Sonner>
+  <ClientOnly>
+    <Sonner
+      :class="cn('toaster group', props.class)"
+      v-bind="props"
+      :theme="$colorMode.preference as Theme"
+    >
+      <template #success-icon>
+        <Icon name="lucide:circle-check" class="size-4" />
+      </template>
+      <template #info-icon>
+        <Icon name="lucide:info" class="size-4" />
+      </template>
+      <template #warning-icon>
+        <Icon name="lucide:triangle-alert" class="size-4" />
+      </template>
+      <template #error-icon>
+        <Icon name="lucide:octagon-x" class="size-4" />
+      </template>
+      <template #loading-icon>
+        <div>
+          <Icon name="lucide:loader" class="size-4 animate-spin" />
+        </div>
+      </template>
+      <template #close-icon>
+        <Icon name="lucide:x" class="size-4" />
+      </template>
+    </Sonner>
+  </ClientOnly>
 </template>
